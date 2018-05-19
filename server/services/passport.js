8ab86fff10aac42d744d19a8da0ae747af7b3104
luisalvarez <alvarezluis121@gmail.com>
2018-05-19 10:57:24 -0700

    Directory change

---
+++ server/services/passport.js
@@ -0,0 +1,44 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const mongoose = require('mongoose');
+const keys = require("../config/keys");
+
+const User = mongoose.model('users');
+
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); //user in this method is a mongoose user model
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
+
+passport.use(
+  new GoogleStrategy({
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({
+          googleId: profile.id
+        })
+        .then((existingUser) => {
+          if (existingUser) {
+            //we have a record for the user
+            done(null, existingUser);
+          } else {
+
+            new User({
+                googleId: profile.id
+              }).save()
+              .then(user => done(null, user));
+          }
+        });
+    }
+  )
+);
